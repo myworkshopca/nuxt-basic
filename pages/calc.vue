b59aa7d74a5899147303c6859ad96d35e1a06ebc
Sean Chen <sean.chen@leocorn.com>
2021-08-25 19:30:02 -0400

    start play around vuetify button component

--- pages/calc.vue
+++ pages/calc.vue
@@ -1,26 +1,37 @@
 <template lang="pug">
-table
-
-  // the display row
-  tr
-    th(colspan="4") 00000000
-
-  tr
-    td 7
-    td 8
-    td 9
-    td X
-
-  tr
-    td 4
-    td 5
-    td 6
-    td -
-
-  tr
-    td 1
-    td 2
-    td 3
-    td +
+v-app
+  table
+  
+    // the display row
+    tr
+      th(colspan="4") 00000000
+  
+    tr
+      td 7
+      td 8
+      td 9
+      td X
+  
+    tr
+      td 4
+      td 5
+      td 6
+      td
+        v-btn(
+          fab
+          color="warning"
+        )
+          v-icon mdi-minus
+  
+    tr
+      td 1
+      td 2
+      td 3
+      td
+        v-btn(
+          fab
+          color="warning"
+        )
+          v-icon mdi-plus
 
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template lang="pug">
-div
+v-app
   h1 Hello World
   h2 Hello second world
+
+  v-row(
+    align="center"
+    justify="space-around"
+  )
+    // try the button component.
+    v-btn(
+      color="primary"
+    ) Normal
+
+    v-btn(
+      color="success"
+    ) Success
+
+    v-btn(
+      fab
+      dark
+      color="error"
+    )
+      v-icon(
+        dark
+      ) mdi-ab-testing
 </template>

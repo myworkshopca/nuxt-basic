390113897912239171cc2010ba5d1ab085361c00
Sean Chen <sean.chen@leocorn.com>
2021-08-25 18:51:37 -0400

    add new page for calculator

---
+++ pages/calc.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+table
+
+  // the display row
+  tr
+    th(colspan="4") 00000000
+
+  tr
+    td 7
+    td 8
+    td 9
+    td X
+
+  tr
+    td 4
+    td 5
+    td 6
+    td -
+
+  tr
+    td 1
+    td 2
+    td 3
+    td +
+
+</template>

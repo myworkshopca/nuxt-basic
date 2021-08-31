7ed5dab9bb29744b26563bf74144104f2e598e36
Sean Chen <sena.chen@leocorn.com>
2021-08-31 16:00:39 -0400

    minor update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
 export default {
 
-  buildModules: [
-    '@nuxtjs/vuetify',
-  ]
+    server: {
+        //port: 80, // default is 3000
+        port: 3000,
+        host: '0.0.0.0' // default is localhost
+    },
+  
+    buildModules: [
+        '@nuxtjs/vuetify',
+    ]
 }

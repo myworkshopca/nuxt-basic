da563bf0fbfa440a4253b7df1b9024bf8604f63b
Sean Chen <sena.chen@leocorn.com>
2021-08-31 14:49:32 -0400

    update to use v-for

--- pages/calc.vue
+++ pages/calc.vue
@@ -1,37 +1,146 @@
 <template lang="pug">
 v-app
-  table
-  
-    // the display row
-    tr
-      th(colspan="4") 00000000
-  
-    tr
-      td 7
-      td 8
-      td 9
-      td X
-  
-    tr
-      td 4
-      td 5
-      td 6
-      td
-        v-btn(
-          fab
-          color="warning"
-        )
-          v-icon mdi-minus
-  
-    tr
-      td 1
-      td 2
-      td 3
-      td
-        v-btn(
-          fab
-          color="warning"
-        )
-          v-icon mdi-plus
-
+    v-card(
+        class = "mx-auto"
+        max-width = "500"
+        outlined
+    )
+        table
+            // display the result.
+            tr
+              th(colspan = "4")
+                v-text-field(
+                    filled
+                    clearable
+                )
+            // first row 7, 8, 9
+            tr
+              td( 
+                v-for="n in [7, 8, 9]"
+                :key="n"
+              )
+                v-btn(
+                  large
+                  fab
+                  color = "deep-purple darken-4"
+                ) 
+                  v-icon(
+                      color = "grey lighten-5"
+                  ) {{ "mdi-numeric-" + n + "-circle" }}
+              td 
+                v-btn(
+                 large
+                 fab
+                 color = "red lighten-1"
+                ) 
+                    v-icon mdi-close
+            // second row: 4, 5, 6
+            tr
+              td( 
+                v-for="n in [4, 5, 6]"
+                :key="n"
+              )
+                v-btn(
+                  large
+                  fab
+                  color = "deep-purple darken-4"
+                ) 
+                  v-icon(
+                      color = "grey lighten-5"
+                  ) {{ "mdi-numeric-" + n + "-circle" }}
+              td 
+                v-btn(
+                 large
+                 fab
+                 color = "red lighten-1"
+                ) 
+                    v-icon mdi-minus
+            tr
+              td( 
+                v-for="n in [1, 2, 3]"
+                :key="n"
+              )
+                v-btn(
+                  large
+                  fab
+                  color = "deep-purple darken-4"
+                ) 
+                  v-icon(
+                      color = "grey lighten-5"
+                  ) {{ "mdi-numeric-" + n + "-circle" }}
+              td 
+                v-btn(
+                 large
+                 fab
+                 color = "red lighten-1"
+                ) 
+                    v-icon mdi-plus
+            tr
+              td 
+                v-btn(
+                 large
+                 fab
+                 color = "deep-purple darken-4"
+                ) 
+                    v-icon(
+                        color = "grey lighten-5"
+                    ) mdi-numeric-0-circle
+              td 
+                v-btn(
+                 large
+                 fab
+                 color = "deep-purple darken-4"
+                )
+                    v-icon(
+                        color = "grey lighten-5"
+                    ) mdi-circle-small
+              td 
+                v-btn(
+                  large
+                  fab
+                  color = "deep-purple darken-4"
+                  outlined
+                )
+                    v-icon mdi-arrow-left-bold
+              td 
+                v-btn(
+                 large
+                 fab
+                 color = "red lighten-1"
+                ) 
+                    v-icon mdi-slash-forward
+            tr
+              //td
+              //  v-btn(
+              //    large
+              //    color = "green accent-4"
+              //    fab
+              //  )
+              //      v-icon mdi-plus-minus-variant 
+              //td
+              //  v-btn(
+              //    large
+              //    fab
+              //    color = "red lighten-1"
+              //  )
+              //      v-icon mdi-exponent
+              //td
+              th(colspan = "4")
+                v-btn(
+                 large 
+                 color = "red lighten-1"
+                 block
+                ) 
+                    v-icon(
+                       large
+                    ) mdi-equal
 </template>
+<script>
+export default {
+    data: function() {
+        return {
+            iconName: 'mdi-numeric-7-circle'
+        };
+    }
+}
+</script>
